4fe38bb381b10bd73289c0c6c9059d3cd51b6d82
Aldrick Handinata <aldrickhandinata1@gmail.com>
2018-11-25 22:48:26 +0700

    Added Class Library for Item

---
+++ Stock Library/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock_Library
+{
+    class Item
+    {
+        public string itemID { get; set; }
+        public string itemName { get; set; }
+        public int qty { get; set; }
+    }
+}
---
+++ Stock Library/Stock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock_Library
+{
+    class Stock
+    {
+        public string transactionID { get; set; }
+        public DateTime transactionDate { get; set; }
+        public Item itemData { get; set; }
+        public int qtyBefore { get; set; }
+        public int qtyAfter { get; set; }
+        public int itemIN { get; set; }
+        public int itemOUT { get; set; }
+    }
+}
